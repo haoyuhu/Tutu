e7b817ad78a4cfc7668644cbd732c5eeb5d3ef78
HaoyuHu <im@huhaoyu.com>
2015-11-28 17:46:04 +0800

    utils: add utils

---
+++ tutu/src/main/java/com/huhaoyu/tutu/utils/TutuConstants.java
@@ -0,0 +1,20 @@
+package com.huhaoyu.tutu.utils;
+
+import mu.lab.thulib.thucab.CabConstants;
+
+/**
+ * Constants for tutu
+ * Created by coderhuhy on 15/11/27.
+ */
+public class TutuConstants {
+
+    public interface Constants {
+        long REFRESH_INTERVAL = 10 * CabConstants.DateTimeConstants.MILLIS_OF_SECOND
+                * CabConstants.DateTimeConstants.SECOND_OF_MINUTE;
+    }
+
+    public interface BundleKey {
+
+    }
+
+}
